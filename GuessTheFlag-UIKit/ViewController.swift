549fb6229c6de567cda7c4d1a5123f222342149e
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-07-29 23:54:34 +0300

    import images, build ui in story builder

--- GuessTheFlag-UIKit/ViewController.swift
+++ GuessTheFlag-UIKit/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstFlag: UIButton!
+    @IBOutlet weak var secondFlag: UIButton!
+    @IBOutlet weak var thirdFlag: UIButton!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
